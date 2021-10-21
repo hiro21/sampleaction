dfa03582403467d8518d463cd1ef4fb50402d222
hiro21 <hiswing8@gmail.com>
2021-10-21 20:26:41 +0900

    web系のアノテーションを使えるようにspring-webの依存を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework:spring-web:5.3.11")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/hiswing/sampleaction/api/HelloController.kt
+++ src/main/kotlin/com/hiswing/sampleaction/api/HelloController.kt
@@ -1,4 +1,7 @@
 package com.hiswing.sampleaction.api
 
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HelloController {
 }
